c72ee478c845fb200bc241ef0bd2c075d5e7c7ba
Peter <85614837+underSkord@users.noreply.github.com>
2024-03-18 17:48:33 +0100

    add small animation to contacts when hovered

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -5,9 +5,15 @@ function ContactComponent() {
     <div className={styles.contactContainer}>
       <div>You can reach me here</div>
       <div className={styles.contacts}>
-        <p>EMAIL goes here</p>
-        <p>LINKEDIN goes here</p>
-        <p>GITHUB goes here</p>
+        <p>
+          <a href="mailto:pivonpeter@gmail.com">Email</a>
+        </p>
+        <p>
+          <a href="https://hu.linkedin.com/in/peter-pivon-9036a8276">LinkedIn</a>
+        </p>
+        <p>
+          <a href="https://github.com/PeterPiv">GitHub</a>
+        </p>
       </div>
       <div>
         Or send me a message <strong>directly</strong>
