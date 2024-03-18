d480630a4865ccbfe1c77f1f0f2ff13473a0544b
Peter <85614837+underSkord@users.noreply.github.com>
2024-03-18 17:14:11 +0100

    add contacts skeleton content

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -1,7 +1,19 @@
 import styles from "./ContactComponent.module.css";
 
 function ContactComponent() {
-  return <div>This is the contact component</div>;
+  return (
+    <div className={styles.contactContainer}>
+      <div>You can reach me here</div>
+      <div className={styles.contacts}>
+        <p>EMAIL goes here</p>
+        <p>LINKEDIN goes here</p>
+        <p>GITHUB goes here</p>
+      </div>
+      <div>
+        Or send me a message <strong>directly</strong>
+      </div>
+    </div>
+  );
 }
 
 export default ContactComponent;
