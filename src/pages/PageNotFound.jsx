e1a0aae675c8f93fa58a55db8a92ae169bfdefdc
Peter <85614837+underSkord@users.noreply.github.com>
2024-04-14 12:04:02 +0200

    add NavLink to Homepage for easier usage

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,18 @@
+import { NavLink } from "react-router-dom";
+import styles from "./PageNotFound.module.css";
+
 function PageNotFound() {
-  return <div>Page not found. 🚫</div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.pageNotFoundText}>That page does not exist. </h2>
+      <p className={styles.backToHome}>
+        Go back to the homepage:
+        <div>
+          <NavLink to="/">Home</NavLink>
+        </div>
+      </p>
+    </div>
+  );
 }
 
 export default PageNotFound;
